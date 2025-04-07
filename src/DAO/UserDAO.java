696d8c4f662148ec0d8a7f16dffd5391f361f971
Yuuta0504 <Bachnd2006@gmail.com>
2025-04-07 10:32:38 +0700

    Sua UserDAO

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Period;
 
 public class UserDAO {
     public static boolean CheckLogin(String email, String password) {
@@ -17,6 +18,20 @@ public class UserDAO {
             return rs.next();
 
         } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static boolean ForgetPassword(String email) {
+        String query = "Select * From users Where email = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement mt = connection.prepareStatement(query)){
+            mt.setString(1, email);
+            ResultSet rs = mt.executeQuery();
+
+            return rs.next();
+        }
+        catch (Exception e) {
             e.printStackTrace();
             return false;
         }
